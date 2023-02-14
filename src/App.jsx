22193fa15f75d0f7a245edbd20a0737ab6adc62c
Vangelis Mazis <mazhs1992@hotmail.com>
2023-02-14 10:26:15 +0200

    Props for component Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Post from './components/Post'
 
 function App() {
   
 
-  return (
-    <h1>Hello World!</h1>
+  return (<>
+    <h1>Posts</h1>
+    <Post author='Vangelis' body='Mazis Body' />
+    <Post author='Marx' body='Marx Body'/>
+    <Post/>
+    </>  
   )
 }
 
---
+++ src/components/Post.jsx
@@ -0,0 +1,14 @@
+
+
+function Post(props) {
+
+return (<div className="Post">
+    <p>{props.author}</p>
+    <p>{props.body}</p>
+</div>)
+}
+  
+
+
+
+export default Post

b700c5ee369940b7a6b60ad5f2672dd818bb537a
Vangelis Mazis <mazhs1992@hotmail.com>
2023-02-24 13:30:00 +0200

    React Routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import NewPost from './components/NewPost'
+import { RouterProvider,createBrowserRouter } from "react-router-dom";
 import './index.css'
 
+//here you pass all the routes you want to have
+const router =createBrowserRouter([
+  {path:'/',element:  <App />},
+  {path:'/test',element: <h1>test</h1>},
+  {path:'/create-post',element:  <NewPost />}
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
+  
   </React.StrictMode>,
 )
+
+/*
+
+RouterProvider
+    tells React to watch the url and send different component for different path
+
+*/
